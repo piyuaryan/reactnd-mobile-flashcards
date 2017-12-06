d43c02bfe419c307ce4c100a86c9efa65a943bec
Piyush Ramavat <piyush@gyrix.com.au>
2017-12-06 20:47:35 +1100

    Added APIs and Notification helpers

---
+++ utils/api.js
@@ -0,0 +1,43 @@
+import {AsyncStorage} from "react-native";
+import {DECK_STORAGE_KEY} from "./helpers";
+
+export function fetchAllDecks() {
+    return AsyncStorage.getItem(DECK_STORAGE_KEY)
+        .then((results) => {
+            return JSON.parse(results)
+        })
+}
+
+export function fetchDeck(title) {
+    return AsyncStorage.getItem(DECK_STORAGE_KEY)
+        .then((results) => {
+            const data = JSON.parse(results);
+            return data[title]
+        })
+}
+
+export function saveDeckTitle(title) {
+    return AsyncStorage.mergeItem(DECK_STORAGE_KEY, JSON.stringify({[title]: {title: title, questions: []}}))
+}
+
+export function addCardToDeck(title, question) {
+    return AsyncStorage.getItem(DECK_STORAGE_KEY)
+        .then((results) => {
+            const data = JSON.parse(results);
+            if (data[title] !== undefined) {
+                data[title].questions.push(question);
+                AsyncStorage.setItem(DECK_STORAGE_KEY, JSON.stringify(data))
+            }
+        })
+}
+
+export function removeDeck(deck) {
+    return AsyncStorage.getItem(DECK_STORAGE_KEY)
+        .then((results) => {
+            const data = JSON.parse(results);
+            data[deck] = undefined;
+            delete data[deck];
+            AsyncStorage.setItem(DECK_STORAGE_KEY, JSON.stringify(data))
+        })
+}
+
